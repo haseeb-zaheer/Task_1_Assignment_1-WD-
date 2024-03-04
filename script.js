e1abcbe6e501ecec68407ea1e2173c63db45276e
Haseeb Zaheer <62466015+haseeb-zaheer@users.noreply.github.com>
2024-03-04 19:53:10 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function animateContent()
+{
+    console.log("yes");
+    document.getElementById('displayText');
+    displayText.remove();
+    document.querySelector('.roboto-slab-container').classList.remove('hidden');
+    document.querySelector('.roboto-slab-container2').classList.remove('hidden');
+    doocument.querySelector('.smallBox').classList.add('hidden');
+}
